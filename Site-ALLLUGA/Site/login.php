a96fc594ccf8ed412b7dac3280d02de21ba834ff
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2023-05-05 01:48:49 -0300

    Add files via upload

---
+++ Site-ALLLUGA/Site/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>Formulário de login</title>
+</head>
+<body>
+	<nav>
+		<button><a href="cadastro.php">Voltar</a></button>
+		<button><a href="principal.php">Home</a></button>
+	</nav>
+    <div class="box">
+        <form method="post" action="testaLogin.php">
+            <fieldset>
+                <legend><b>Login de Locador</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="login" id="login" class="inputUser" required>
+                    <label for="login" class="labelInput">Login</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="password" class="labelInput">Senha</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="cpf" id="cpf" class="inputUser" required>
+                    <label for="cpf" class="labelInput">CPF</label>
+                </div>
+                <div>
+                    <label for="">Escolha: </label>
+                    <select name="escolha" required>
+                        <option value="2">Locador</option>
+                        <option value="1">administrador</option>
+                    </select>
+                </div>
+                <br><br>
+                <input type="submit" name="submit" id="submit" value="Entrar">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
